78a249e635939cb94c3e06dd398fc95f13d07949
Flechashe <se.amigos@gmail.com>
2022-06-20 19:08:43 -0300

    Rehice la página -Hice un template base.html -Implementé Bootstrap -Listado y creación en el índice

--- patients/urls.py
+++ patients/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'patients'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('registrar-paciente/', views.register_patient),
     # path('pacientes/', views.list, name='list'),
     path('pacientes/', views.ListView.as_view(), name='list'),
     path('pacientes/<int:pk>/', views.DetailView.as_view(), name='detail'),
--- patients/views.py
+++ patients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
 from django.template import loader
@@ -7,9 +7,24 @@ from .models import Patient
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Bienvenido al índice.")
 
+def index(request):
+    patients = Patient.objects.all()
+    return render(request, 'patients/index.html', {'patients': patients})
+
+
+def register_patient(request):
+    nombre = request.POST['nombre']
+    apellido = request.POST['apellido']
+    dni = request.POST['dni']
+    sexo = request.POST['sexo']
+    email = request.POST['email']
+    fecha_de_nacimiento = request.POST['fecha_de_nacimiento']
+    cobertura_medica = request.POST['cobertura_medica']
+
+    patient = Patient.objects.create(nombre=nombre, apellido=apellido, dni=dni, sexo=sexo,
+                                     email=email, fecha_de_nacimiento=fecha_de_nacimiento, cobertura_medica=cobertura_medica)
+    return redirect('/')
 
 # def list(request):
 #     patient_list = Patient.objects.all()
@@ -22,6 +37,7 @@ def index(request):
 
 class ListView(generic.ListView):
     model = Patient
+
 
 class DetailView(generic.DetailView):
     model = Patient
