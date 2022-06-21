b1d67a27d63294ef5d130707342ad05db254a798
Flechashe <se.amigos@gmail.com>
2022-06-21 02:49:52 -0300

    Implementé edición y eliminación de pacientes

--- patients/urls.py
+++ patients/urls.py
@@ -5,7 +5,10 @@ from . import views
 app_name = 'patients'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('registrar-paciente/', views.register_patient),
+    path('registrar-paciente/', views.process_register_patient),
+    path('editar-paciente/<int:pk>/', views.edit_patient),
+    path('procesar-edicion/<int:pk>', views.process_edit_patient),
+    path('eliminar-paciente/<int:pk>/', views.delete_patient),
     # path('pacientes/', views.list, name='list'),
     path('pacientes/', views.ListView.as_view(), name='list'),
     path('pacientes/<int:pk>/', views.DetailView.as_view(), name='detail'),
--- patients/views.py
+++ patients/views.py
@@ -13,7 +13,7 @@ def index(request):
     return render(request, 'patients/index.html', {'patients': patients})
 
 
-def register_patient(request):
+def process_register_patient(request):
     nombre = request.POST['nombre']
     apellido = request.POST['apellido']
     dni = request.POST['dni']
@@ -25,6 +25,39 @@ def register_patient(request):
     patient = Patient.objects.create(nombre=nombre, apellido=apellido, dni=dni, sexo=sexo,
                                      email=email, fecha_de_nacimiento=fecha_de_nacimiento, cobertura_medica=cobertura_medica)
     return redirect('/')
+
+
+def edit_patient(request, pk):
+    patient_to_edit = Patient.objects.get(id=pk)
+    return render(request, 'patients/edit_patient.html', {'patient': patient_to_edit})
+
+
+def process_edit_patient(request, pk):
+    nombre = request.POST['nombre']
+    apellido = request.POST['apellido']
+    dni = request.POST['dni']
+    sexo = request.POST['sexo']
+    email = request.POST['email']
+    fecha_de_nacimiento = request.POST['fecha_de_nacimiento']
+    cobertura_medica = request.POST['cobertura_medica']
+
+    patient_to_edit = Patient.objects.get(id=pk)
+    patient_to_edit.nombre = nombre
+    patient_to_edit.apellido = apellido
+    patient_to_edit.dni = dni
+    patient_to_edit.sexo = sexo
+    patient_to_edit.email = email
+    patient_to_edit.fecha_de_nacimiento = fecha_de_nacimiento
+    patient_to_edit.cobertura_medica = cobertura_medica
+    patient_to_edit.save()
+    return redirect('/')
+
+
+def delete_patient(request, pk):
+    patient_to_delete = Patient.objects.get(id=pk)
+    patient_to_delete.delete()
+    return redirect('/')
+
 
 # def list(request):
 #     patient_list = Patient.objects.all()
